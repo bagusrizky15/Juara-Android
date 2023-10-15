63e8416cfa548272bf5cbf6c49a4de48020970f8
Joshua Allan <joshua_saputra@bca.co.id>
2023-10-15 13:55:22 +0700

    add jetpack compose praktek

---
+++ 3. Jetpack Compose Praktek/app/src/main/java/com/bgzss/jetpackcompose/MainActivity.kt
@@ -0,0 +1,85 @@
+package com.bgzss.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.bgzss.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ArticleApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ArticleApp(){
+    ImageBackground(
+        title = stringResource(id = R.string.title_jetpack_compose_tutorial),
+        shortDescription = stringResource(id = R.string.compose_short_desc),
+        longDescription = stringResource(id = R.string.compose_long_desc),
+        image = painterResource(id = R.drawable.bg_compose_background),
+    )
+}
+
+@Composable
+fun ImageBackground(
+    title: String,
+    shortDescription: String,
+    longDescription: String,
+    modifier: Modifier = Modifier,
+    image: Painter
+    ) {
+    Column(modifier = Modifier) {
+        Image(painter = image, contentDescription = null)
+        Text(
+            text = title,
+            modifier = Modifier.padding(16.dp),
+            fontSize = 24.sp
+        )
+        Text(
+            text = shortDescription,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = longDescription,
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeTheme {
+        ArticleApp()
+    }
+}
